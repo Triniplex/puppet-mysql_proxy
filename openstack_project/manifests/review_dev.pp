openstack-infra/system-config

Make the gitweb links in gerrit point to git.o.o
Gerrit has builtin support for cgit links. Use it - but test it out on
review-dev first.

Change-Id: I8ea38e08258cdc8eb95e2fc3c1da5a4dc8faee57

modules/openstack_project/manifests/review_dev.pp
@@ -59,6 +59,9 @@ class openstack_project::review_dev (
       'trivial-rebase@review-dev.openstack.org',
     email_private_key               => $email_private_key,
     sysadmins                       => $sysadmins,
+    gitweb                          => false,
+    cgit                            => true,
+    cgit_url                        => 'https://git.openstack.org/cgit',
     swift_username                  => $swift_username,
     swift_password                  => $swift_password,
     replication                     => [

Triniplex/puppet-mysql_proxy

openstack_project/manifests/review_dev.pp
@@ -59,6 +59,9 @@ class openstack_project::review_dev (
       'trivial-rebase@review-dev.openstack.org',
     email_private_key               => $email_private_key,
     sysadmins                       => $sysadmins,
+    gitweb                          => false,
+    cgit                            => true,
+    web_repo_url                    => 'https://git.openstack.org/cgit',
     swift_username                  => $swift_username,
     swift_password                  => $swift_password,
     replication                     => [