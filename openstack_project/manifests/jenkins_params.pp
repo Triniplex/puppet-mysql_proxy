openstack-infra/system-config

Be able to generate math formulae with Sphinx mathpng base extension.
Change-Id: If9f53894d449bf54f7dcefe33c8f7597843a0438

modules/openstack_project/manifests/jenkins_params.pp
@@ -75,11 +75,13 @@ class openstack_project::jenkins_params {
           Package['cgroups'],
           Package['cgroups-tools'],
         ]
+        $dvipng = 'dvipng'
       } else {
         $mysql_dev_package = 'mysql-devel'
         $cgroups_tools_package = ''
         $cgconfig_require = Package['cgroups']
         $cgred_require = Package['cgroups']
+        $dvipng = 'texlive-dvipng'
       }
     }
     'Debian': {
@@ -91,6 +93,7 @@ class openstack_project::jenkins_params {
       $docbook_xml_package = 'docbook-xml'
       $docbook5_xml_package = 'docbook5-xml'
       $docbook5_xsl_package = 'docbook-xsl'
+      $dvipng = 'dvipng'
       $firefox_package = 'firefox'
       $graphviz_package = 'graphviz'
       $libcurl_dev_package = 'libcurl4-gnutls-dev'

modules/openstack_project/manifests/thick_slave.pp
@@ -12,6 +12,7 @@ class openstack_project::thick_slave(
     $::openstack_project::jenkins_params::awk_package, # for building extract_docs.awk to work correctly
     $::openstack_project::jenkins_params::asciidoc_package, # for building gerrit/building openstack docs
     $::openstack_project::jenkins_params::curl_package,
+    $::openstack_project::jenkins_params::dvipng, # for generating image (ie: math) in docs
     $::openstack_project::jenkins_params::docbook_xml_package, # for building openstack docs
     $::openstack_project::jenkins_params::docbook5_xml_package, # for building openstack docs
     $::openstack_project::jenkins_params::docbook5_xsl_package, # for building openstack docs

Triniplex/puppet-mysql_proxy

openstack_project/manifests/jenkins_params.pp
@@ -75,11 +75,13 @@ class openstack_project::jenkins_params {
           Package['cgroups'],
           Package['cgroups-tools'],
         ]
+        $dvipng_package = 'texlive-dvipng'
       } else {
         $mysql_dev_package = 'mysql-devel'
         $cgroups_tools_package = ''
         $cgconfig_require = Package['cgroups']
         $cgred_require = Package['cgroups']
+        $dvipng_package = 'dvipng'
       }
     }
     'Debian': {
@@ -91,6 +93,7 @@ class openstack_project::jenkins_params {
       $docbook_xml_package = 'docbook-xml'
       $docbook5_xml_package = 'docbook5-xml'
       $docbook5_xsl_package = 'docbook-xsl'
+      $dvipng_package = 'dvipng'
       $firefox_package = 'firefox'
       $graphviz_package = 'graphviz'
       $libcurl_dev_package = 'libcurl4-gnutls-dev'

openstack_project/manifests/thick_slave.pp
@@ -12,6 +12,7 @@ class openstack_project::thick_slave(
     $::openstack_project::jenkins_params::awk_package, # for building extract_docs.awk to work correctly
     $::openstack_project::jenkins_params::asciidoc_package, # for building gerrit/building openstack docs
     $::openstack_project::jenkins_params::curl_package,
+    $::openstack_project::jenkins_params::dvipng_package, # for generating image (ie: math) in docs
     $::openstack_project::jenkins_params::docbook_xml_package, # for building openstack docs
     $::openstack_project::jenkins_params::docbook5_xml_package, # for building openstack docs
     $::openstack_project::jenkins_params::docbook5_xsl_package, # for building openstack docs